8769b4a1cb732ba60f04d3bf53406e237a4bb66a
John Tomlinson <johnt84@hotmail.co.uk>
2020-05-06 00:09:07 +0100

    * Migrated Blazor To-Do List app to .Net Core 3.1 * Saves and restore changes to the to-do list * Strikeshrough the to-do item when its marked as complete

---
+++ BlazorToDoList.App/Services/FileService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorToDoList.App.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BlazorToDoList.App.Services
+{
+    public class FileService : IFileService
+    {
+        private readonly IConfiguration _configuration;
+
+        public FileService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string ReadFromFile()
+        {
+            return File.ReadAllText(_configuration["SampleDataFile"]);
+        }
+
+        public void SaveToFile(List<ToDoItem> toDoItems)
+        {
+            string json = JsonConvert.SerializeObject(toDoItems);
+            System.IO.File.WriteAllText(_configuration["SampleDataFile"], json);
+        }
+    }
+}
--- BlazorToDoList.App/Startup.cs
+++ BlazorToDoList.App/Startup.cs
@@ -1,17 +1,59 @@
-using Microsoft.AspNetCore.Components.Builder;
+using BlazorToDoList.App.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace BlazorToDoList.App
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        public void Configure(IComponentsApplicationBuilder app)
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
         {
-            app.AddComponent<App>("app");
+            services.AddRazorPages();
+            services.AddServerSideBlazor()
+                .AddCircuitOptions(options =>
+                {
+                    //can toggle detailed errors on or off from app settings
+                    options.DetailedErrors = Convert.ToBoolean(Configuration["DetailedErrors"]);
+                });
+
+            services.AddTransient<IFileService, FileService>();
+            services.AddScoped<IToDoService, ToDoService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
         }
     }
 }
